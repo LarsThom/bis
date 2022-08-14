1096518b1c168e0073e9058ca64dca8149f0da13
Lars Thomsen <lars.thomsen@student.uni-halle.de>
2022-08-14 17:25:09 +0200

    cleanup

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
-from logging import critical
 import sys
 import json
 from docplex.mp.model import Model
 from docplex.mp.relax_linear import LinearRelaxer
 
-# set accuracy and alacrity goals
+# read accuracy, alacrity and implants
 
-GOAL_ACCURACY = 2694
-GOAL_ALACRITY = 2100
-
-# set implants: 'alacrity' or 'critical'
-IMPLANT1 = 'alacrity'
-IMPLANT2 = 'critical'
+GOAL_ACCURACY = int(input("Enter Accuracy: "))
+GOAL_ALACRITY = int(input("Enter Alacrity: "))
+print("Implants: either critical(also crit, c) or alacrity(also alac, a")
+IMPLANT1 = input("Enter Implant 1: ").lower()
+IMPLANT2 = input("Enter Implant 2: ").lower()
 
 # define model
 m = Model(name='SWTOR 7.1')
@@ -38,24 +36,27 @@ StimsAccuracy = m.binary_var(name='accuracy-stim')
 
 CrystalsCritical = m.integer_var(name='critical-crystal')
 
-if IMPLANT1 == 'alacrity':
-    if IMPLANT2 == 'alacrity':
+list_critical = ['critical', 'crit', 'c']
+list_alacrity = ['alacrity', 'alac', 'a']
+
+if IMPLANT1 in list_alacrity:
+    if IMPLANT2 in list_alacrity:
         Implants1Alacrity = 1
         Implants1Critical = 0
         Implants2Alacrity = 1
         Implants2Critical = 0
-    elif IMPLANT2 == 'critical':
+    elif IMPLANT2 in list_critical:
         Implants1Alacrity = 1
         Implants1Critical = 0
         Implants2Alacrity = 0
         Implants2Critical = 1
-elif IMPLANT1 == 'critical':
-    if IMPLANT2 == 'alacrity':
+elif IMPLANT1 in list_critical:
+    if IMPLANT2 in list_alacrity:
         Implants1Alacrity = 0
         Implants1Critical = 1
         Implants2Alacrity = 1
         Implants2Critical = 0
-    elif IMPLANT2 == 'critical':
+    elif IMPLANT2 in list_critical:
         Implants1Alacrity = 0
         Implants1Critical = 1
         Implants2Alacrity = 0
@@ -64,20 +65,21 @@ else:
     sys.exit('ERROR: ILLEGAL IMPLANTS')
 
 
+
 # read datafile and set correct values
-with open("./data.json", "r") as read_file:
-    data = json.load(read_file)
+# with open("./data.json", "r") as read_file:
+#     data = json.load(read_file)
 
-accuracy_dict = {"Enhancements":0, "Augments":0, "Earpieces":0, "Stims":0}
-alacrity_dict = {"Enhancements":0, "Augments":0, "Earpieces":0, "Implants":0}
-critical_dict = {"Enhancements":0, "Augments":0, "Earpieces":0, "Implants":0, "Crystals":0}
+# accuracy_dict = {"Enhancements":0, "Augments":0, "Earpieces":0, "Stims":0}
+# alacrity_dict = {"Enhancements":0, "Augments":0, "Earpieces":0, "Implants":0}
+# critical_dict = {"Enhancements":0, "Augments":0, "Earpieces":0, "Implants":0, "Crystals":0}
 
-for x in accuracy_dict:
-    accuracy_dict[x] = data[x][0]['Accuracy']
-for x in alacrity_dict:
-    alacrity_dict[x] = data[x][0]['Alacrity']
-for x in critical_dict:
-    critical_dict[x] = data[x][0]['Critical']
+# for x in accuracy_dict:
+#     accuracy_dict[x] = data[x][0]['Accuracy']
+# for x in alacrity_dict:
+#     alacrity_dict[x] = data[x][0]['Alacrity']
+# for x in critical_dict:
+#     critical_dict[x] = data[x][0]['Critical']
 
 # [accuracy_dict[x] = (data[x][0]['Accuracy']) for x in accuracy_dict]
 # [alacrity_dict[x] = (data[x][0]['Alacrity']) for x in alacrity_dict]
@@ -87,10 +89,30 @@ for x in critical_dict:
 # print(alacrity_dict)
 # print(critical_dict)
 
+
+
 # set constraints
-setAccuracy = m.sum([554*EnhancementsAccuracy, 130*AugmentsAccuracy, 554*EarpiecesAccuracy, 264*StimsAccuracy])
-setAlacrity = m.sum([554*EnhancementsAlacrity, 130*AugmentsAlacrity, 554*EarpiecesAlacrity, 577*Implants1Alacrity, 577*Implants2Alacrity])
-setCritical = m.sum([554*EnhancementsCritical, 130*AugmentsCritical, 554*EarpiecesCritical, 577*Implants1Critical, 577*Implants2Critical, 41*CrystalsCritical])
+setAccuracy = m.sum([
+    554*EnhancementsAccuracy,
+    130*AugmentsAccuracy,
+    554*EarpiecesAccuracy,
+    264*StimsAccuracy
+    ])
+setAlacrity = m.sum([
+    554*EnhancementsAlacrity,
+    130*AugmentsAlacrity,
+    554*EarpiecesAlacrity,
+    577*Implants1Alacrity,
+    577*Implants2Alacrity
+    ])
+setCritical = m.sum([
+    554*EnhancementsCritical,
+    130*AugmentsCritical,
+    554*EarpiecesCritical,
+    577*Implants1Critical,
+    577*Implants2Critical,
+    41*CrystalsCritical
+    ])
 
 CONSTRAINT_ACCURACY = m.add_constraint(setAccuracy >= GOAL_ACCURACY, ctname='accuracy-constraint')
 
@@ -108,10 +130,12 @@ CONSTRAINT_CRYSTAL = m.add_constraint(m.sum([CrystalsCritical]) == 2, ctname='ea
 
 
 # goal:
-m.set_multi_objective('min', [264*StimsAccuracy + 554*EnhancementsAccuracy + 130*AugmentsAccuracy + 554*EarpiecesAccuracy, 554*EnhancementsAlacrity + 130*AugmentsAlacrity + 554*EarpiecesAlacrity + 577*Implants1Alacrity + 577*Implants2Alacrity])
+m.set_multi_objective('min', [264*StimsAccuracy + 554*EnhancementsAccuracy + 130*AugmentsAccuracy + 554*EarpiecesAccuracy,
+    554*EnhancementsAlacrity + 130*AugmentsAlacrity + 554*EarpiecesAlacrity + 577*Implants1Alacrity + 577*Implants2Alacrity])
 
 # solve and display
 m.solve().display()
+print("\n")
 print("\nSolution values:")
 print("Accuracy: ", setAccuracy.solution_value)
 print("Alacrity: ", setAlacrity.solution_value)
